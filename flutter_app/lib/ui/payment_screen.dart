7b60d075d3de548b515139be83f5f170b399fe3e
momchiloiliev <momchilo.iliev@students.finki.ukim.mk>
2023-12-10 00:30:15 +0100

    Updated Payment screen

--- flutter_app/lib/ui/payment_screen.dart
+++ flutter_app/lib/ui/payment_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/ui/appbar.dart';
 import 'package:flutter_app/ui/button.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'order_confirmed_screen.dart';
 
 class PaymentScreen extends StatefulWidget {
   const PaymentScreen({super.key});
@@ -10,55 +12,199 @@ class PaymentScreen extends StatefulWidget {
 }
 
 class _PaymentScreenState extends State<PaymentScreen> {
+  bool isLightTheme = true;
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  bool useGlassMorphism = false;
+  bool useBackgroundImage = false;
+  bool useFloatingAnimation = true;
+  final OutlineInputBorder border = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.black.withOpacity(0.7),
+      width: 2.0,
+    ),
+  );
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(
-        title: "Payment",
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          // Text style for text fields' input.
+          titleMedium: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.white,
+          background: Colors.white,
+          // Defines colors like cursor color of the text fields.
+          primary: Colors.blue,
+        ),
+        // Decoration theme for the text fields.
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: const TextStyle(color: Colors.blue),
+          labelStyle: const TextStyle(color: Colors.blue),
+          focusedBorder: border,
+          enabledBorder: border,
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  Text("cscsv"),
-                  Text("cscsv"),
-                  Text("cscsv"),
-                ],
-              ),
-              Column(
-                children: [
-                  Text("cscsv"),
-                  Text("cscsv"),
-                  Text("cscsv"),
-                ],
-              ),
-            ],
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    Text("jsbvbsivb"),
-                    Text("chbsvbsh"),
-                    Row(
-                      children: [
-                        Text("data"),
-                        Text("data"),
+
+      home: Scaffold(
+        appBar: const CustomAppBar(title: "Payment"),
+        resizeToAvoidBottomInset: false,
+        body: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    CreditCardWidget(
+                      enableFloatingCard: useFloatingAnimation,
+                      glassmorphismConfig: _getGlassmorphismConfig(),
+                      cardNumber: cardNumber,
+                      expiryDate: expiryDate,
+                      cardHolderName: cardHolderName,
+                      cvvCode: cvvCode,
+                      bankName: 'Axis Bank',
+                      showBackView: isCvvFocused,
+                      obscureCardNumber: true,
+                      obscureCardCvv: true,
+                      isHolderNameVisible: true,
+                      cardBgColor: isLightTheme
+                          ? Colors.blue
+                          : Colors.blue,
+                      isSwipeGestureEnabled: true,
+                      onCreditCardWidgetChange:
+                          (CreditCardBrand creditCardBrand) {},
+                      customCardTypeIcons: <CustomCardTypeIcon>[
+                        CustomCardTypeIcon(
+                          cardType: CardType.mastercard,
+                          cardImage: Image.asset(
+                            'images/card_images/mastercard.png',
+                            height: 48,
+                            width: 48,
+                          ),
+                        ),
                       ],
-                    )
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: <Widget>[
+                            CreditCardForm(
+                              formKey: formKey,
+                              obscureCvv: true,
+                              obscureNumber: true,
+                              cardNumber: cardNumber,
+                              cvvCode: cvvCode,
+                              isHolderNameVisible: true,
+                              isCardNumberVisible: true,
+                              isExpiryDateVisible: true,
+                              cardHolderName: cardHolderName,
+                              expiryDate: expiryDate,
+                              inputConfiguration: const InputConfiguration(
+                                cardNumberDecoration: InputDecoration(
+                                  labelText: 'Number',
+                                  hintText: 'XXXX XXXX XXXX XXXX',
+                                ),
+                                expiryDateDecoration: InputDecoration(
+                                  labelText: 'Expired Date',
+                                  hintText: 'XX/XX',
+                                ),
+                                cvvCodeDecoration: InputDecoration(
+                                  labelText: 'CVV',
+                                  hintText: 'XXX',
+                                ),
+                                cardHolderDecoration: InputDecoration(
+                                  labelText: 'Card Holder',
+                                ),
+                              ),
+                              onCreditCardModelChange: onCreditCardModelChange,
+                            ),
+                            const SizedBox(height: 20),
+                            const SizedBox(height: 20),
+                            GestureDetector(
+                              onTap: _onValidate,
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 8,
+                                ),
+                                decoration: const BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                padding:
+                                const EdgeInsets.symmetric(vertical: 15),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'Pay 3600MKD',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'halter',
+                                    fontSize: 14,
+                                    package: 'flutter_credit_card',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
-                )
-              ],
-            ),
-          ),
-          BlueButton(text: "Pay", route: "/orderConfirmed")
-        ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
+  }
+
+  void _onValidate() {
+    if (formKey.currentState?.validate() ?? false) {
+      print('valid!');
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => OrderConfirmedScreen()),
+      );
+    } else {
+      print('invalid!');
+    }
+  }
+
+  Glassmorphism? _getGlassmorphismConfig() {
+    if (!useGlassMorphism) {
+      return null;
+    }
+
+    final LinearGradient gradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: <Color>[Colors.grey.withAlpha(50), Colors.grey.withAlpha(50)],
+      stops: const <double>[0.3, 0],
+    );
+
+    return isLightTheme
+        ? Glassmorphism(blurX: 8.0, blurY: 16.0, gradient: gradient)
+        : Glassmorphism.defaultConfig();
+  }
+
+  void onCreditCardModelChange(CreditCardModel creditCardModel) {
+    setState(() {
+      cardNumber = creditCardModel.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
   }
 }
